5dbff9e303015540fa1254684814d54f361884ac
lsy20140 <lsy20140@naver.com>
2023-08-12 19:31:17 +0900

    feat: 라우팅 구조 변경

--- groweam/src/index.js
+++ groweam/src/index.js
@@ -3,11 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Todo from './pages/Todo';
+import Meeting from './pages/Meeting';
+import TeamManage from './pages/TeamManage';
+import DashBoard from './pages/DashBoard';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {index: true, element: <Landing/>},
+      {path: '/dashboard', element: <DashBoard/>},
+      {path: '/login', element: <Login/>},
+      {path: '/todo', element: <Todo/>},
+      {path: '/meeting', element: <Meeting/>},
+      {path: '/teamManage', element: <TeamManage/>},
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
--- groweam/src/pages/DashBoard/index.js
+++ groweam/src/pages/DashBoard/index.js
@@ -1,33 +1,24 @@
 import React, { useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import styled from 'styled-components';
-import { useNavigate } from "react-router-dom";
-import Meeting from '../Meeting';
+
 
 const ContentWrapper = styled.div`
     /* position: absolute;  */
 `;
 
-const Home = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-
+const DashBoard = () => {
 
     const { name, accessToken } = useSelector((status) => ({
         name: status.myInfo.name,
         accessToken: status.login.accessToken
     }))
-    
-    const goToMeeing = useCallback(() => {
-        navigate('../Meeting');
-    }, [navigate]);
 
     return (
         <div>
-            Home
-            <button onClick={goToMeeing}></button>
+            dashboard
         </div>
     );
 }
 
-export default Home;
+export default DashBoard;
